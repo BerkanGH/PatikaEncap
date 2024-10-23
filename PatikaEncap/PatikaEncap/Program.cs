843823d983f418c7d54d0a4b383186663665f9a8
BerkanGH <berkankeskin28@gmail.com>
2024-10-23 03:17:02 +0300

    odev

---
+++ PatikaEncap/PatikaEncap/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaEncap
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Araba araba1 = new Araba();
+
+            araba1._marka = "mercedes";
+            araba1.KapıSayisi = 4;
+            araba1._renk = "siyah";
+            araba1._model = "2015 model";
+
+
+            Araba araba2 = new();
+
+            araba2._marka = "audi";
+            araba2.KapıSayisi = 3;
+            araba2._renk = "lacivert";
+            araba2._model = "2020";
+
+
+            
+            
+
+           
+
+
+
+        }
+
+    }
+}
